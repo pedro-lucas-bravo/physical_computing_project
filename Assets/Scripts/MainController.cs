4e5faddfa167d119600d86eb2e3c7de20f835c41
Pedro Lucas <pedro.lucas.bravo@gmail.com>
2020-10-11 00:25:10 -0500

    Make LightSensor work and map with pitch

--- Assets/Scripts/MainController.cs
+++ Assets/Scripts/MainController.cs
@@ -2,17 +2,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class MainController : MonoBehaviour
 {
     public VirtualPianoController virtualPiano;
     public AudioGenerator audioGenerator;
     public SynthModule[] synthChain;
+    public Text lightData;
 
-    //[Header("Audio Balance")]
-    //public float maxAmplitude = 1f;
+    [Header("Audio Balance")]
+    public AnimationCurve pitchCurve;
+    public float minPitchVariation = 0;
+    public float maxPitchVariation = 1;
 
     private void Awake() {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        if (LightSensor.current != null)
+            InputSystem.EnableDevice(LightSensor.current);
+        else
+            lightData.text = "NO";
+
         virtualPiano.OnNoteOn += OnNoteOn;
         virtualPiano.OnNoteOff += OnNoteOff;        
     }
@@ -22,9 +33,21 @@ public class MainController : MonoBehaviour
             virtualPiano.OnNoteOn -= OnNoteOn;
             virtualPiano.OnNoteOff -= OnNoteOff;
         }
+        if (LightSensor.current != null)
+            InputSystem.DisableDevice(LightSensor.current);
+    }
+
+    private void Update() {
+        if (LightSensor.current != null) {
+            light_ = LightSensor.current.lightLevel.ReadValue();
+            lightData.text = light_ + "";
+            audioGenerator.soundOuput.pitch = Mathf.Lerp(minPitchVariation, maxPitchVariation, pitchCurve.Evaluate(Mathf.Clamp(light_, 0, maxLightValue_ + 1 ) / (maxLightValue_ + 1)));
+        }
+
     }
 
     private void Start() {
+        CalibrateLightSensor();
         for (int i = synthChain.Length - 1; i >= 1; i--) {
             synthChain[i].SetSourceModule(synthChain[i - 1]);
         }
@@ -38,4 +61,15 @@ public class MainController : MonoBehaviour
     private void OnNoteOff(int note, int velocity) {
         MIDI.MidiManager.OnNoteOff?.Invoke(note, velocity);
     }
+
+    public void CalibrateLightSensor() {
+        if (LightSensor.current != null) {
+            if (!LightSensor.current.enabled)
+                InputSystem.EnableDevice(LightSensor.current);
+            maxLightValue_ = LightSensor.current.lightLevel.ReadValue();
+        }
+    }
+
+    float light_;
+    float maxLightValue_;
 }
